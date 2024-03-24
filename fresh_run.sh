815820acacc28cdfefd4997529eafa233d4c04c3
dr298 <298story@gmail.com>
2024-03-24 23:57:07 +0700

    Update fresh_run.sh

--- fresh_run.sh
+++ fresh_run.sh
@@ -2,5 +2,5 @@ killall screen
 wget https://raw.githubusercontent.com/elliottophellia/yakumo/master/results/GRASS_GOOD.txt
 rm proxy_list.txt
 mv GRASS_GOOD.txt proxy_list.txt
-./run_screens.sh
+./run_screen.sh
 
